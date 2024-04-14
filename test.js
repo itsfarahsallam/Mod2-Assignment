846f98b1f38ee4554a3288574fc092b4252058c1
Farah Sallam <farahsallam@student.olympic.edu>
2024-04-14 14:37:59 -0700

    Changed h1 and renamed the Javascript file

---
+++ test.js
@@ -0,0 +1,12 @@
+/* 3 Examples of basic arithmetic in JS */
+
+
+/* 2 Examples of declaring variables in JS */
+
+
+/* 2 Examples of using variables in arithmetic operations in JS */
+
+
+/* Practice */
+let message;
+message = "Hello World";
