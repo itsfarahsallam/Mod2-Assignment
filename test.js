f881e14bfeaa80e5d8f1185e26a91d78f0e28584
Farah Sallam <farahsallam@student.olympic.edu>
2024-04-14 15:59:36 -0700

    Wrote in the document using Javascript, and created a random number generator

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
+console.log("Hello World");
+document.write("Hello World <br>");
+document.write("Hello World Again <br><br><br>");
+
 /* 3 Examples of basic arithmetic in JS */
+document.write
 
-
-/* 2 Examples of declaring variables in JS */
+/* 3 Examples of declaring variables in JS */
 
 
 /* 2 Examples of using variables in arithmetic operations in JS */
@@ -24,3 +28,6 @@ function changeColor()
         box.style.background = 'red';
     }  
 }
+
+document.write("Random Number Generator (1-10) <br> (Refresh the page if you want a new number!): <br><br>")
+document.write(Math.round(Math.random() *10));
