e6d62eb49edd894c7676b065abf737a7fef59166
Farah Sallam <farahsallam@student.olympic.edu>
2024-04-14 15:10:57 -0700

    Created a Javascript function that changes the color of the box

--- test.js
+++ test.js
@@ -13,6 +13,14 @@
 var button = document.querySelector('button');
 var box = document.getElementById('changeMe');
 
-button.onclick = function changeColor(){
-    box.style.background = 'blue';
+function changeColor()
+{
+    if(box.style.background == 'red')
+    {
+        box.style.background = 'blue';
+    }
+    else
+    {
+        box.style.background = 'red';
+    }  
 }
